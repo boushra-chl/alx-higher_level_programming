6e880e81d760f670c8a36dc5ddb2625a0f32b821
boushra-chl <bchilouch1998@gmail.com>
2024-01-18 21:29:07 +0000

    fix3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0 import add as addition
-result = addition(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
