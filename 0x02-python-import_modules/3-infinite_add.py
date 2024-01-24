1b37686b05bf05fd04263addf2269c23bf10c1f9
boushra-chl <bchilouch1998@gmail.com>
2024-01-24 17:56:56 +0100

    add infinite

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    add = 0
+    i = 1
+    if len(sys.argv) - 1 == 0:
+        add = 0
+    while i <= len(sys.argv) - 1:
+        add = add + int(sys.argv[i])
+        i = i + 1
+    print("{:d}".format(add))
+
+
