74e4619f308a4705c4c7512b8d6f701b56c086e2
boushra-chl <bchilouch1998@gmail.com>
2024-01-27 19:10:32 +0100

    different elements

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/udr/bin/python3
+def only_diff_elements(set_1, set_2):
+    diff_elements = set()
+    for x in set_1:
+        if x not in set_2:
+            diff_elements.add(x)
+    for x in set_2:
+        if x not in set_1:
+            diff_elements.add(x)
+    return diff_elements
