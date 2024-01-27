ece02f4caa0947fdbd7fc0213f705dce983207bf
boushra-chl <bchilouch1998@gmail.com>
2024-01-27 13:50:30 +0100

    pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sqrt_matrix =[]
+    sqrt_matrix = []
     for row in matrix:
         sqrt_row = [x**2 for x in row]
         sqrt_matrix.append(sqrt_row)
-    return sqrt_matrix    
+    return sqrt_matrix
