9744a0245b4592fdc56d0458acf7cb00f81efd06
boushra-chl <bchilouch1998@gmail.com>
2024-01-28 18:07:13 +0000

    multiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    keys_copy = list(a_dictionary.keys())
     n_dictionary = {}
-    for k in keys_copy:
-        for v in a_dictionary:
-            n_dictionary[k] = v**2
+    for k, v in a_dictionary.items():
+        n_dictionary[k] = v * 2
     return n_dictionary
