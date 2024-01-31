f6788e5707920a1263c916037b4816298f2e3627
boushra-chl <bchilouch1998@gmail.com>
2024-01-31 18:37:42 +0000

    fix

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * check_cycle - function that checks for a cycle
+ * @list: pointer to listint_t
+ *
+ * Return: 1 if a cycle exists and 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+
