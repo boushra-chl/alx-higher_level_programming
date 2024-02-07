42a042923a0b4d428eae8a7539cf0a42993fb4ee
boushra-chl <bchilouch1998@gmail.com>
2024-02-07 13:35:56 +0100

    exceptions

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.8
+if __name__ == "__main__":
+    import hidden_4
+    defined_names = dir(hidden_4)
+    for name in defined_names:
+        if name[:2] != '__':
+            print(name)
