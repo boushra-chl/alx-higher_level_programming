150001079638516df1f2fd737f099fe5451b2948
boushra-chl <bchilouch1998@gmail.com>
2024-02-07 15:27:12 +0000

    fix2

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         result = None
     finally:
         if result != None:
