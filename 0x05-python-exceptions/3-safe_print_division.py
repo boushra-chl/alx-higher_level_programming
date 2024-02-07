fd32d8021bd4523086e56a4485a217874a982d0b
boushra-chl <bchilouch1998@gmail.com>
2024-02-07 14:49:00 +0000

    print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        if result != None:
+            print("inside result: {:f}".format(result))
+        else:
+            print("inside result: None")
+    return result
