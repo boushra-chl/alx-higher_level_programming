433b313af9b44b8e3e5b59ea24d2c66e54ca0e7f
boushra-chl <bchilouch1998@gmail.com>
2024-02-20 18:39:58 +0000

    add items

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list, and then save them to a file"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+arguments = sys.argv
+args = arguments[1:]
+save_to_json_file(args, "add_item.json")
+data = load_from_json_file("add_item.json")
