99a5e99125c1448da0526081e4f118cc73a34782
boushra-chl <bchilouch1998@gmail.com>
2024-03-02 13:55:56 +0000

    rectangle8:staticmethod

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -63,3 +63,12 @@ class Rectangle:
             return ""
         return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
 
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        elif rect_2.area() > rect_1.area():
+            return rect_2
