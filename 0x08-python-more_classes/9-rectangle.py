34b5d17cdbd171a5350b2cb3aaf36dd89e252c5f
boushra-chl <bchilouch1998@gmail.com>
2024-03-02 14:17:31 +0000

    rectangle9

---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""module that defines a rectangle"""
+
+
+class Rectangle:
+    """class that defines a real rectangle"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+        self.print_symbol = Rectangle.print_symbol
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (2 * (self.width + self.height))
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle_str = ""
+        for _ in range(self.height):
+            rectangle_str += (str(self.print_symbol) * self.width) + '\n'
+        return rectangle_str.rstrip('\n')
+
+    def __repr__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        elif rect_2.area() > rect_1.area():
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
