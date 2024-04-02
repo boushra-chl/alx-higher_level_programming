efce26e6e6784b4f04c0911412d884ca1bf00c92
coushra-chl <bchilouch1998@gmail.com>
2024-04-02 14:14:37 +0000

    21

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create user and database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_06_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_06_2'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
