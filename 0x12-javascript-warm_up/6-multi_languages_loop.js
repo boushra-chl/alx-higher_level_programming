b27e8c12fefdae47d8f2a95a7e1450df91b52a2d
boushra-chl <bchilouch1998@gmail.com>
2024-04-22 22:28:48 +0100

    6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+script that prints element of an array
+*/
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
