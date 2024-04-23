c17bb215a61d742ddfb640977d0f9d5a94d9dff2
coushra-chl <bchilouch1998@gmail.com>
2024-04-23 13:28:08 +0100

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+prints a square
+*/
+const argument = process.argv;
+const num = Number(argument[2]);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    row = '';
+    for (let j = 0; j < num; j++) {
+      row = row + 'X';
+    }	    
+      console.log(row);
+  }
+}
