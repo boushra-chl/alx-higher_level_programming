d2ac91241a8df0badc8e5770c51e43d624515388
coushra-chl <bchilouch1998@gmail.com>
2024-04-26 13:16:39 +0100

    24

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (h, w) {
     if (h <= 0 || w <= 0 || isNaN(w) || isNaN(h)) {
-       return new Rectangle();
+      return {};
     }
     this.width = w;
     this.height = h;
