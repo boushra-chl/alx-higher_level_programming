81af1f1535243d0ac0d3e0e0b0b1f1271c262fd7
boushra-chl <128801718+boushra-chl@users.noreply.github.com>
2024-05-22 22:15:29 +0100

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,3 +0,0 @@
--- Create user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++
@@ -1,7 +0,0 @@
---  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_show_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_show_genres.genre
-ORDER BY number_of_shows DESC;
