944abf029f81daffb08a4721353694b67762cd91
boushra-chl <bchilouch1998@gmail.com>
2024-06-20 16:59:26 +0100

    js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const BaseSquare = require('./5-square');
+class Square extends BaseSquare {
+  charPrint(c) {
+    let raw = '';
+    if (c === undefined) {
+      c = 'x';
+    }
+    for (let i = 0; i < this.width; i++) {
+      raw = raw + c;
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(raw);
+    }
+  }
+}
+
+module.exports = Square;
